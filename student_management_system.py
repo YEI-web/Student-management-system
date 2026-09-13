6032ec64c5e4a73d311d45e7c3d0fe3684c37a74
YEI <3568931724@qq.com>
2026-09-13 22:04:32 +0800

    学生管理系统框架2

--- student_management_system.py
+++ student_management_system.py
@@ -1,3 +1,15 @@
+def add():
+    print("添加学生信息")
+def delete():
+    print("删除学生信息")
+def modify():
+    print("修改学生信息")
+def query():
+    print("查询学生信息")
+def show():
+    print("显示学生信息")
+
+
 def main():
     while True:
         print('''
@@ -11,6 +23,22 @@ def main():
                 ''')
 
         option=int(input("请输入你选择的操作："))
+        if option==1:
+            add()
+        elif option==2:
+            delete()
+        elif option==3:
+            modify()
+        elif option==4:
+            query()
+        elif option==5:
+            show()
+        elif option==0:
+            break
+        else:
+            print("输入错误，请重新输入")
+
+
 
 if __name__=='__main__':
     main()
